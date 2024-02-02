87f0ada3069d0d73d6216a99b47e8bcffada1113
khaouti nouhaila <khaoutinouhaila@gmail.com>
2024-02-02 16:22:34 +0100

    adding the oauth 2 configuration

--- src/main/java/com/example/taskflow/config/SecurityConfig.java
+++ src/main/java/com/example/taskflow/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
--- src/main/java/com/example/taskflow/config/SecurityFilterConfig.java
+++ src/main/java/com/example/taskflow/config/SecurityFilterConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static com.example.taskflow.domaine.Permission.*;
 import static com.example.taskflow.domaine.Role.ADMIN;
@@ -38,6 +43,8 @@ public class SecurityFilterConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors((cors) -> cors
+                        .configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req.requestMatchers("/api/v1/auth/**")
@@ -53,6 +60,8 @@ public class SecurityFilterConfig {
                                 .requestMatchers(DELETE, "/api/v1/concour/**").hasAnyAuthority(CONCOUR_DELETE.name())
                                 .anyRequest()
                                 .authenticated()
+/*                                .and()
+                                .oauth2Login()*/
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
@@ -65,5 +74,18 @@ public class SecurityFilterConfig {
         ;
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()  {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200/"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        configuration.setAllowedHeaders(Arrays.asList(
+                "Accept", "Origin", "Content-Type", "Depth", "User-Agent", "If-Modified-Since,",
+                "Cache-Control", "Authorization", "X-Req", "X-File-Size", "X-Requested-With", "X-File-Name"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
